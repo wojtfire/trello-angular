21d644e6d85bf1847255ff1545826e9ba1275d2e
Wojciech Jakubek <jakubekwojciech@gmail.com>
2019-04-26 10:55:46 +0200

    Adding basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { ListsComponent } from './lists/lists.component';
+import { ListItemComponent } from './lists/list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    ListsComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule
