10cb8e10ecaa71bff86f7677f0eb8bd6302b4ae4
Wojciech Jakubek <jakubekwojciech@gmail.com>
2019-05-14 13:59:47 +0200

    List, list-items styling added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
 import { ListsComponent } from './lists/lists.component';
-import { ListItemComponent } from './lists/list-item/list-item.component';
+import { ListItemComponent } from './lists/list/list-item/list-item.component';
+import { ListComponent } from './lists/list/list.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { ListItemComponent } from './lists/list-item/list-item.component';
     NavComponent,
     HeaderComponent,
     ListsComponent,
-    ListItemComponent
+    ListItemComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
