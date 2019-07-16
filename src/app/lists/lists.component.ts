590c698a461de0fc2a40619072f211bbed0a75cc
Wojciech Jakubek <jakubekwojciech@gmail.com>
2019-07-16 21:54:16 +0200

    Lists changes, new list creation

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, OnInit } from '@angular/core';
+import { Component, Output, OnInit, EventEmitter } from '@angular/core';
 import { List } from '../model/list.model';
 import { ListItem } from '../model/list-item.model';
 
@@ -18,31 +18,35 @@ export class ListsComponent implements OnInit {
 
   ngOnInit() {
     this.lists[0].items.push({
+      name: 'Think about UI for personal website bla bla bla',
+      description: 'Choose template, colors, animations.',
+      comment: '',
+      priority: 'green'
+    });
+    this.lists[0].items.push({
       name: 'Think about UI for personal website',
       description: 'Choose template, colors, animations.',
       comment: '',
-      priority: 'red'
+      priority: 'yellow'
     });
     this.lists[0].items.push({
       name: 'Think about UI for personal website',
       description: 'Choose template, colors, animations.',
-      comment: ''
-    });
-    this.lists[0].items.push({
-      name: 'Think about UI for personal website',
-      description: 'Choose template, colors, animations.',
-      comment: ''
+      comment: '',
+      priority: 'blue'
     });
 
     this.lists[1].items.push({
       name: 'Think about UI for personal website',
       description: 'Choose template, colors, animations.',
-      comment: ''
+      comment: '',
+      priority: 'purple'
     });
     this.lists[1].items.push({
       name: 'Think about UI for personal website',
       description: 'Choose template, colors, animations.',
-      comment: ''
+      comment: '',
+      priority: 'orange'
     });
     this.lists[1].items.push({
       name: 'Think about UI for personal website',
@@ -93,4 +97,6 @@ export class ListsComponent implements OnInit {
         });
     }
   }
+
+  onAddList() {}
 }
